52b53174dad748459f10be67c702246c36f55c43
Matteo <imatteomazzone@gmail.com>
2021-11-17 19:49:21 -0500

    Added a few features
    Actually looks ok now 
    Reset button
    Optimization of Game of life rules
    Var to Let changes
    Optimization of neighbour count algorithm

--- cell.js
+++ cell.js
@@ -2,95 +2,97 @@
 //2- Any live cell with two or three live neighbours lives on to the next generation.
 //3- Any live cell with more than three live neighbours dies, as if by overpopulation.
 //4- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-
 function Cell(i, j, w) {
-  this.i = i;
-  this.j = j;
-  this.x = i * w;
-  this.y = j * w;
-  this.w = w;
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
+    this.w = w;
 
-  this.neighbourCount = 0;
+    this.neighbourCount = 0;
 
-  this.alive = false;
+    this.alive = false;
 
 }
 
 Cell.prototype.show = function() {
 
-  stroke(0);
-  noFill();
-  rect(this.x, this.y, this.w, this.w);
+    noStroke();
+    noFill();
+    rect(this.x, this.y, this.w, this.w);
 
 
     if (this.alive) {
-      fill(255);
-      ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
+        fill(255);
+        noStroke();
+        rect(this.x, this.y, this.w, this.w);
 
-/*
-        textAlign(CENTER);
-        fill(0);
-        text(this.neighbourCount, this.x + this.w * 0.5, this.y + this.w - 6);
-*/
+
+        //textAlign(CENTER);
+        //fill(0);
+        //text(this.neighbourCount, this.x + this.w * 0.5, this.y + this.w - 6);
+
 
     } else {
-      fill(100);
-      rect(this.x, this.y, this.w, this.w);
+        fill(100);
+        noStroke();
+        rect(this.x, this.y, this.w, this.w);
 
-      /*
-      textAlign(CENTER);
-        fill(0);
-        text(this.neighbourCount, this.x + this.w * 0.5, this.y + this.w - 6);
-      */
+
+        //textAlign(CENTER);
+        //fill(0);
+        //text(this.neighbourCount, this.x + this.w * 0.5, this.y + this.w - 6);
+
     }
 
 };
 
+
 Cell.prototype.countNeighbours = function() {
-  if (this.alive){
-    var total = -1;
-  }
-  else{
-    var total = 0;
-  }
-  for (var xoff = -1; xoff <= 1; xoff++) {
-    var i = this.i + xoff;
-    if (i < 0 || i >= cols) continue;
 
-    for (var yoff = -1; yoff <= 1; yoff++) {
-      var j = this.j + yoff;
-      if (j < 0 || j >= rows) continue;
+    let total = 0;
 
-      var neighbour = grid[i][j];
-      if (neighbour.alive) {
-        total++;
-      }
+    for (let xoff = -1; xoff <= 1; xoff++) {
+        let i = this.i + xoff;
+        if (i < 0 || i >= cols) continue;
+
+        for (let yoff = -1; yoff <= 1; yoff++) {
+            let j = this.j + yoff;
+            if (j < 0 || j >= rows) continue;
+
+            if (xoff === 0 && yoff === 0) continue;
+
+
+            let neighbour = grid[i][j];
+            if (neighbour.alive) {
+                total++;
+            }
+        }
     }
-  }
-  this.neighbourCount = total;
+    this.neighbourCount = total;
 };
 
 
 Cell.prototype.golRules = function() {
 
-//Conditions 1,2,3 require a living cell
-if (this.alive){
-  //Condition 1
-  if (this.neighbourCount <2){
-    this.alive = false;
-  }
-  //Condition 2 (Should not change anything if cell is alive)
-  else if (this.neighbourCount === 2 || this.neighbourCount === 3) {
-    this.alive = true;
-  }
-  //Condition 3
-  else if (this.neighbourCount > 3) {
-    this.alive = false;
-  }
+    //Conditions 1,2,3 require a living cell
+    if (this.alive) {
+
+        if (this.neighbourCount < 2 || this.neighbourCount > 3) {
+            this.alive = false;
+        }
+
+    }
+    //Condition 4 is for dead cells
+    else if (this.neighbourCount === 3) {
+        this.alive = true;
+    }
 }
-  //Condition 4
-  else if (!this.alive && this.neighbourCount === 3) {
-    this.alive = true;
-  }
+
+
+Cell.prototype.isAlive = function() {
+    if (this.alive = true)
+        return true;
+    else
+        return false;
 }
--- sketch.js
+++ sketch.js
@@ -1,89 +1,128 @@
 function make2DArray(cols, rows) {
-  var arr = new Array(cols);
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = new Array(rows);
-  }
-  return arr;
+    var arr = new Array(cols);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
 }
 
 var grid;
 var cols;
 var rows;
-var w = 20;
+var w = 10;
 
-var totalAlive = 5000;
+var totalAlive = 5100;
+
+var currentAlive = 0;
 
 
-var delayInMilliseconds = 1000; //1 second
 
-
+let button;
 
 function setup() {
-  createCanvas(3001, 3001);
-  frameRate(30);
-  cols = floor(width / w);
-  rows = floor(height / w);
+    createCanvas(1700, 800);
+    frameRate(60);
 
-  grid = make2DArray(cols, rows);
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Cell(i, j, w);
-    }
-  }
 
-  // Pick totalAlive spots
-  var options = [];
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      options.push([i, j]);
-    }
-  }
 
-  for (var n = 0; n < totalAlive; n++) {
-    var index = floor(random(options.length));
-    var choice = options[index];
-    var i = choice[0];
-    var j = choice[1];
-    // Deletes that spot so it's no longer an option
-    options.splice(index, 1);
-    grid[i][j].alive = true;
-  }
 
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].countNeighbours();
-    }
-  }
+    cols = floor(1301 / w);
+    rows = floor(801 / w);
 
-  setInterval(function() {
-    //Game Of Life Logic
+    grid = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            grid[i][j].golRules();
+            grid[i][j] = new Cell(i, j, w);
         }
     }
 
-    //Re-count neighbours
+    // Pick totalAlive spots
+    var options = [];
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            options.push([i, j]);
+        }
+    }
+
+    for (let n = 0; n < totalAlive; n++) {
+        var index = floor(random(options.length));
+        var choice = options[index];
+        var i = choice[0];
+        var j = choice[1];
+        // Deletes that spot so it's no longer an option
+        options.splice(index, 1);
+        grid[i][j].alive = true;
+    }
+
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             grid[i][j].countNeighbours();
         }
     }
 
-  }, 100);
+    setInterval(function() {
+        //Game Of Life Logic
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                grid[i][j].golRules();
+            }
+        }
+
+        //Re-count neighbours
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                grid[i][j].countNeighbours();
+            }
+        }
+
+    }, 300);
+
+    button = createButton('Reset Board');
+    button.position(1380, 180);
+    button.mousePressed(setup);
 
 }
 
 
 
+
 function draw() {
-  background(255);
+
+    background(255);
+
+    fill(128, 128, 128);
+    noStroke();
+    rect(1300, 0, 30, 800);
 
     //Display changes
-    for (var i = 0; i < cols; i++) {
-      for (var j = 0; j < rows; j++) {
-        grid[i][j].show();
-      }
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+
+            if (grid[i][j].alive === true) {
+                currentAlive++;
+            }
+
+            grid[i][j].show();
+        }
     }
+
+
+    textSize(30);
+    text("Conway's Game Of Life", 1370, 50);
+    textSize(20);
+
+
+    text(totalAlive, 1570, 130);
+    text("Initial Alive Cells: ", 1370, 130);
+
+
+    text(currentAlive, 1570, 150);
+    text("Current Alive Cells: ", 1370, 150);
+
+
+
+
+    currentAlive = 0;
 
 }
